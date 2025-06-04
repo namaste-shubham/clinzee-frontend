c39849ca3633c720922a1fc7234c7af4f43e053d
shubham.kumar <shubham.11kumar1998@gmail.com>
2025-06-04 22:01:58 +0530

    fix: protect sensitive keys

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCjmjpVwbQidcFimWWrvz3r5LMlsRvXjsA",
-  authDomain: "clinzee-7bfb9.firebaseapp.com",
-  projectId: "clinzee-7bfb9",
-  storageBucket: "clinzee-7bfb9.firebasestorage.app",
-  messagingSenderId: "1020280497375",
-  appId: "1:1020280497375:web:0f62a04ce9b362a468f56a",
-  measurementId: "G-WXELRWQWR0",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
