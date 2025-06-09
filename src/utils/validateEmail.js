2e5ea8dab165b78af13db54017881255c7bec9e5
Shubham Kumar <shubham20.kumar@lge.com>
2025-06-09 16:18:29 +0900

    fix: use mailboxlayer pi

--- src/utils/validateEmail.js
+++ src/utils/validateEmail.js
@@ -1,14 +1,18 @@
 import axios from "axios";
 
 export const isValidEmailDomain = async (email) => {
-  const API_KEY = import.meta.env.VITE_VALIDATE_EMAIL_API_KEY; // Replace this
-  const url = `https://emailvalidation.abstractapi.com/v1/?api_key=${API_KEY}&email=${email}`;
+  const API_KEY = import.meta.env.VITE_MAILBOXLAYER_API_KEY;
+  const url = `https://apilayer.net/api/check?access_key=${API_KEY}&email=${email}&smtp=1&format=1`;
 
   try {
     const res = await axios.get(url);
-    return res.data.is_valid_format.value && res.data.is_smtp_valid.value;
+    console.log("email validation check:::", res.data);
+    const { format_valid, mx_found, smtp_check } = res.data;
+
+    // ✅ All three should be true for a solid check
+    return format_valid && mx_found && smtp_check;
   } catch (err) {
-    console.error("Email validation error:", err);
+    console.error("Mailboxlayer error:", err);
     return false;
   }
 };
